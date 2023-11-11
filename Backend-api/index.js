9de82ccdad0e379875ebce12adcad2e44ba409e5
Its-SheldonDev <marius.bzcn@gmail.com>
2023-11-11 01:45:40 +0100

    Authentication api creation 🔒

---
+++ Backend-api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env" });
+
+const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/error");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/nuitdelune", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("> DB connection successful ... ");
+  });
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(errorHandler);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`> NuitDeLune is running on: ${PORT} ...`);
+});
